057aca7483de6be8b9880f7aaded999ac4cdfb25
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-04-07 19:16:28 +0300

    feat: added more array filling setups & new comparsion

--- binarySearch(Olog2n).ts
+++ binarySearch(Olog2n).ts
@@ -1,11 +1,30 @@
 // setup
-let arr: number[] = []
+let arr100El: number[] = []
 
+// random array
+// for (let i: number = 0; i <= 100; i++) {
+//     arr100El.push(Math.floor(Math.random() * 100))
+// }
+
+// ordered array
 for (let i: number = 0; i <= 100; i++) {
-    arr.push(Math.floor(Math.random() * 100))
+    arr100El.push(i)
 }
 
-arr.sort((a, b) => a - b)
+let arr1e7El: number[] = []
+
+// random array
+// for (let i: number = 0; i <= 1e7; i++) {
+//     arr1e7El.push(Math.floor(Math.random() * 100))
+// }
+
+// ordered array
+for (let i: number = 0; i <= 1e7; i++) {
+    arr1e7El.push(i)
+}
+
+arr100El.sort((a, b) => a - b)
+arr1e7El.sort((a, b) => a - b)
 
 interface SearchElementFn {
     (
@@ -38,12 +57,12 @@ const binarySearch: SearchElementFn = (arr, item) => {
 const forSearch: SearchElementFn = (arr, item) => {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] == item) {
-            console.log(`the element you are looking for is in the position ${i}`)
+            console.log('\x1b[42m%s\x1b[0m', `the element you are looking for is in the position ${i}`)
             return i
         }
     }
 
-    console.log('there is no such element in the array')
+    console.log('\x1b[41m\x1b[1m\x1b[37m%s\x1b[0m', 'there is no such element in the array')
     return null
 }
 
@@ -58,14 +77,29 @@ const forEachSearch: SearchElementFn = (arr, item) => {
 }
 
 // actually comparsion
-console.time('for search')
-forSearch(arr, 4)
-console.timeEnd('for search')
+console.log('\x1b[34m%s\x1b[0m', 'comparsion for 100 elements')
+console.time('for search 100 elements')
+forSearch(arr100El, 4)
+console.timeEnd('for search 100 elements')
 
-console.time('forEach search')
-forEachSearch(arr, 4)
-console.timeEnd('forEach search')
+console.time('forEach search 100 elements')
+forEachSearch(arr100El, 4)
+console.timeEnd('forEach search 100 elements')
 
-console.time('Binary search')
-binarySearch(arr, 4)
-console.timeEnd('Binary search')
+console.time('Binary search 100 elements')
+binarySearch(arr100El, 4)
+console.timeEnd('Binary search 100 elements')
+
+
+console.log('\x1b[34m%s\x1b[0m', 'comparsion for 10 million elements')
+console.time('for search 1e7 elements')
+forSearch(arr1e7El, 1e5)
+console.timeEnd('for search 1e7 elements')
+
+console.time('forEach search 1e7 elements')
+forEachSearch(arr1e7El, 1e5)
+console.timeEnd('forEach search 1e7 elements')
+
+console.time('Binary search 1e7 elements')
+binarySearch(arr1e7El, 1e5)
+console.timeEnd('Binary search 1e7 elements')
